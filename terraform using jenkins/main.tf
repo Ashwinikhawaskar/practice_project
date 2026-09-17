cd2c247adbfe365e62c0a4f62aa14c72fb83c033
Ashwini Bansod <khawaskarashwini@gmail.com>
2026-09-17 11:57:06 +0530

    terraform ec2

---
+++ terraform using jenkins/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "jenkins_instance" {
+ami_id="ami-07a00cf47dbbc844c"
+key_name=""
+instance_type="t3.micro"
+vpc_security_group_ids="sg-03faeefc1b0ed7d30"
+}
