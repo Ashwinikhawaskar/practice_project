4e5b2a4931e37d847e49169665a3905f382fee8b
Ashwinikhawaskar <123471943+Ashwinikhawaskar@users.noreply.github.com>
2026-09-17 13:22:05 +0530

    Update main.tf

--- terraform_using_jenkins/main.tf
+++ terraform_using_jenkins/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_instance" "jenkins_instance" {
-ami_id="ami-07a00cf47dbbc844c"
-key_name=""
+ami="ami-07a00cf47dbbc844c"
+key_name="private_key"
 instance_type="t3.micro"
-vpc_security_group_ids="sg-03faeefc1b0ed7d30"
+vpc_security_group_ids= ["sg-03faeefc1b0ed7d30"]
 }
